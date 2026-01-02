82f13a7c48fed976207e26069ab61eddb5957bbb
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2026-01-01 17:17:07 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//*****************************************************************************
+//** 961. N-Repeated Element in Size 2N Array                       leetcode **
+//*****************************************************************************
+//** A crowd of numbers, twice in size,                                      **
+//** One lone value wears a clever disguise.                                 **
+//** We count each step till truth is seen,                                  **
+//** The one repeated n times breaks the routine.                            **
+//*****************************************************************************
+
+int repeatedNTimes(int* nums, int numsSize) {
+    int n = numsSize / 2;
+    int count[100000] = {0};
+    for(int i = 0; i < numsSize; i++)
+    {
+        count[nums[i]]++;
+        if(count[nums[i]] == n) return nums[i];
+    }
+    return -1;
+}
